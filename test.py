767af9e9db1666d4e15b7e07e1320eab19f87517
jeffikeMBA <148511046+jeffikeMBA@users.noreply.github.com>
2023-11-09 20:18:44 -0300

    Atualizando

--- test.py
+++ test.py
@@ -16,4 +16,4 @@ class Test(unittest.TestCase):
 
     def test_conteudo(self):
         # verifica o retorno do conteudo da pagina
-        self.assertEqual(self.result.data.decode('utf-8'), "APP Teste Ike - V1")
+        self.assertEqual(self.result.data.decode('utf-8'), "APP Teste Ike - v1")
